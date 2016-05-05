21988cead91480f12378e22a027899986b3323db
masahiro kamono <masahiro.kamono@gmail.com>
2016-05-05 09:58:41 +0000

    second

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,19 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+  
+  def follow(other_user)
+    following_relationships.find_or_create_by(followed_id: other_user.id)
+  end
+
+  # フォローしているユーザーをアンフォローする
+  def unfollow(other_user)
+    following_relationship = following_relationships.find_by(followed_id: other_user.id)
+    following_relationship.destroy if following_relationship
+  end
+
+  # あるユーザーをフォローしているかどうか？
+  def following?(other_user)
+    following_users.include?(other_user)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ActiveRecord::Base
     has_secure_password
     
     has_many :microposts
+    
+    has_many :following_relationships, class_name:  "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent:   :destroy
+    has_many :following_users, through: :following_relationships, source: :followed
 end
